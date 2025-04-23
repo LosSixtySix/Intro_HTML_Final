7392f458f9a74887e849882975086579932dd509
LosSixtySix <112432787+LosSixtySix@users.noreply.github.com>
2025-04-23 11:50:14 -0600

    Created filter for high score page

--- client/highScoreUi.js
+++ client/highScoreUi.js
@@ -5,12 +5,49 @@ const articleSetup  = () => {
     const TitleHeading = document.createElement("h1")
     TitleHeading.textContent = "High Scores"
 
-    ArticleContainer.appendChild(TitleHeading)
+    const playerContainer = document.createElement(`div`)
+    playerContainer.id = "PlayerContainer"
 
+    ArticleContainer.appendChild(TitleHeading)
+    ArticleContainer.appendChild(playerContainer)
     document.body.appendChild(ArticleContainer)
 }
+const Filter = (filterValue,keyPress) =>{
+    if (keyPress.length  === 1)
+    {
+        addPlayers(filterValue+keyPress)
+    }
+    else
+    {
+        const newFilterValue = filterValue.slice(0,-1)
+        addPlayers(newFilterValue)
+    }
+}
+const formSetup = () =>{
+    const navContainer = document.createElement('nav')
+    navContainer.id = `navFormContainerHighScore`
 
-const addPlayers = () =>{
+    const filterLabel = document.createElement('label')
+    filterLabel.htmlFor = 'filter'
+    filterLabel.textContent = 'Filter By Player: '
+
+    const filter = document.createElement('input')
+    filter.type = 'text'
+    filter.id = 'filter'
+
+    const handleFilter = (event) =>{
+        Filter(filter.value,event.key)
+    }
+
+    filter.addEventListener('keydown',handleFilter)
+
+    navContainer.appendChild(filterLabel)
+    navContainer.appendChild(filter)
+
+    document.body.appendChild(navContainer)
+}
+
+const addPlayers = (filterValue) =>{
     const urlString = window.location.href
 
     const paramString = urlString.split(`?`)[1];
@@ -19,20 +56,49 @@ const addPlayers = () =>{
 
     var params = []
 
-    uglyParams.forEach(element => {
-        params.push(element.split(`=`)[0])
-        
-    });
+    if (!filterValue )
+    {
+        uglyParams.forEach(element => {
+            params.push(element.split(`=`)[0])
+        });
+    }
+    else
+    {
+        uglyParams.forEach(element => {
+            const param = element.split(`=`)[0]
+            
+            if(param.includes(filterValue))
+            {
+                params.push(param)
+            }
+        })
+    }
+    
 
-    const ArticleContainer = document.getElementById("HighScoreArticle")
+    
+    const articleContainer = document.getElementById("HighScoreArticle")
+
+    const children = articleContainer.children
+    for(const child of children)
+    {
+        if (child.textContent != "High Scores")
+        {
+            articleContainer.removeChild(child)
+        }
+    }
+    const playerContainer = document.createElement(`div`)
+    playerContainer.id = "PlayerContainer"
+
     params.forEach(element =>{
         const newPlayer = document.createElement("p")
         newPlayer.textContent = `Player: ${element}`
 
-        ArticleContainer.appendChild(newPlayer)
+        playerContainer.appendChild(newPlayer)
     })
 
-}
+    articleContainer.appendChild(playerContainer)
 
+}
+formSetup()
 articleSetup()
-addPlayers()
+addPlayers(null)
